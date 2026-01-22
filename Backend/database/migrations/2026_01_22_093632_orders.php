c6d311f5091543c34dddc16c5766b57daed4d470
Sabrey <limsabriey94@gmail.com>
2026-01-22 16:57:46 +0700

    update

---
+++ Backend/database/migrations/2026_01_22_093632_orders.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_code');
+            $table->foreignId('game_id')->constrained('games')->onDelete('cascade');
+            $table->foreignId('package_id')->constrained('topup_packages')->onDelete('cascade');
+            $table->integer('player_id');
+            $table->integer('server_id ');
+            $table->decimal('price');
+            $table->string('payment_method');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
